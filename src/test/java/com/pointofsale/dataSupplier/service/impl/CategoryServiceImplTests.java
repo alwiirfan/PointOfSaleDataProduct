c42e85f05dcb372cc2df0bc665986ca8a3d60bbc
Mohammad Alwi Irfani <alwi.irfani1927@gmail.com>
2024-02-26 15:18:06 +0700

    [FEATURE] add testing to category product

---
+++ src/test/java/com/pointofsale/dataSupplier/service/impl/CategoryServiceImplTests.java
@@ -0,0 +1,69 @@
+package com.pointofsale.dataSupplier.service.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.pointofsale.dataSupplier.constant.ECategory;
+import com.pointofsale.dataSupplier.entity.Category;
+import com.pointofsale.dataSupplier.repository.CategoryRepository;
+import com.pointofsale.dataSupplier.service.CategoryService;
+
+@SpringBootTest
+public class CategoryServiceImplTests {
+    
+    // MOCK
+    private final CategoryRepository categoryRepository = mock(CategoryRepository.class);
+    private final CategoryService categoryService = new CategoryServiceImpl(categoryRepository);
+
+    @Test
+    void getCategoryOrSave_CategoryExists() {
+        // Arrange
+        ECategory eCategory = ECategory.MAKANAN;
+        Category existingCategory = Category.builder().category(eCategory).build();
+
+        // mock repos behavior
+        when(categoryRepository.findFirstByCategory(eCategory)).thenReturn(Optional.of(existingCategory));
+
+        // Act
+        Category result = categoryService.getOrSave(eCategory);
+
+        //Assert
+        verify(categoryRepository, times(1)).findFirstByCategory(eCategory);
+        verify(categoryRepository, never()).save(any());
+
+        assertEquals(existingCategory.getCategory(), eCategory);
+        assertEquals(existingCategory, result);
+    }
+
+    @Test
+    void getCategoryOrSave_CategoryNotExists() {
+        // Arrange
+        ECategory eCategory = ECategory.MINUMAN;
+        Category newCategory = Category.builder().category(eCategory).build();
+
+        // mock repos behavior
+        when(categoryRepository.findFirstByCategory(eCategory)).thenReturn(Optional.empty());
+        when(categoryRepository.save(any())).thenReturn(newCategory);
+
+        // Act
+        Category result = categoryService.getOrSave(eCategory);
+
+        //Assert
+        verify(categoryRepository, times(1)).findFirstByCategory(eCategory);
+        verify(categoryRepository, times(1)).save(any());
+
+        assertEquals(newCategory.getCategory(), eCategory);
+        assertEquals(newCategory, result);
+    }
+    
+}
